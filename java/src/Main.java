83910b9cfbefb0e16d4602ab470a68c20bfc77a4
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-06-13 16:27:19 +0200

    Extract functions, roles and Entity Types.

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,50 +1,61 @@
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
 import org.docx4j.Docx4J;
-import org.docx4j.TraversalUtil;
-import org.docx4j.finders.ClassFinder;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.R;
+import org.docx4j.wml.Text;
+
 
 public class Main
 {
 
-	static class TraversalCallback extends TraversalUtil.CallbackImpl {
-		@Override
-		public List<Object> apply(Object o) {
-	
-			System.out.println(o.getClass());
-//			org.docx4j.wml.Text textNode = (org.docx4j.wml.Text) o;
-//
-//			String textContent = textNode.getValue();
-//
-//
-//			System.out.println("Found a string: " + textContent);
-
-			//root.appendChild(element);
-
-			return null;
-		}
-
-		@Override
-		public boolean shouldTraverse(Object o) {
-			return true;
-		}
-	}
-	public static void main(String[] args) throws Docx4JException
+	public static Optional<String> runText(R run)
 	{
-		final WordprocessingMLPackage wordMLPackage =	Docx4J.load(new java.io.File("../benchmark/input.docx"));
-		final MainDocumentPart part = wordMLPackage.getMainDocumentPart();
-		new TraversalCallback().walkJAXBElements(part);
-		//final Document doc = (org.docx4j.wml.Document) part.getJaxbElement();
-		//Body body = doc.getBody();
-		//final ClassFinder finder = new ClassFinder(Object.class);
-		//new TraversalUtil(part,finder);
-		//finder.results.stream().forEach(e->{System.out.println(e);});
-
-
-
-
+		return run.getContent()
+				.stream()
+				.map(JAXBElement.class::cast)
+				.map(JAXBElement::getValue)
+				.filter(Text.class::isInstance)
+				.map(Text.class::cast)
+				.map(Text::getValue)
+				.findFirst();
 	}
+	
+	
 
+	public static void main(String[] args) throws Docx4JException, JAXBException
+	{
+		var wordMLPackage =	Docx4J.load(new java.io.File("../benchmark/input.docx"));
+		var doc = wordMLPackage.getMainDocumentPart();
+		//	https://stackoverflow.com/questions/933447/how-do-you-cast-a-list-of-supertypes-to-a-list-of-subtypes
+		// https://stackoverflow.com/questions/56579819/wt-not-an-instance-of-org-docx4j-wml-text/56580036#56580036
+
+		//var boldText = (List<JAXBElement<Text>>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/w:b]/w:t", false)
+		String[][] tagClasses = {{"w:b","Entity Type"},{"w:i","Role"},{"w:u","Function"}};
+
+		for(var tc: tagClasses)
+		{
+			String tag = tc[0];
+			String clazz = tc[1];
+			var runs = (List<R>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/"+tag+"]", false);
+			Set<String> names = runs.stream().map(Main::runText).flatMap(Optional::stream).collect(Collectors.toSet());
+
+			System.out.println("*****************"+names.size()+" "+clazz+"s found********"+names.stream().reduce("", (a,b)->a+"\n"+b));
+			/*
+		for(R run: boldRuns)
+		{
+				runText(run).ifPresent(text->
+				{
+					System.out.println(text);
+				});
+		}
+			 */
+
+		}
+	}
 }
+
