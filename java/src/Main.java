ef924e3895ee2ee561e729694108ead6650916ed
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-06-13 17:12:55 +0200

    Use jena to create Turtle output with types and labels.

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,6 +4,12 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import org.apache.commons.text.WordUtils;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDFS;
 import org.docx4j.Docx4J;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.wml.R;
@@ -12,8 +18,14 @@ import org.docx4j.wml.Text;
 
 public class Main
 {
-
-	public static Optional<String> runText(R run)
+	static final Model MODEL = ModelFactory.createDefaultModel();
+	static final String BB2 = "http://www.snik.eu/ontology/bb2/";
+	static final String META = "http://www.snik.eu/ontology/meta/";
+	static final Resource ENTITY_TYPE = MODEL.createResource(META+"EntityType"); 
+	static final Resource FUNCTION = MODEL.createResource(META+"Function");
+	static final Resource ROLE = MODEL.createResource(META+"Role");
+	
+	static Optional<String> runText(R run)
 	{
 		return run.getContent()
 				.stream()
@@ -26,25 +38,51 @@ public class Main
 	}
 	
 	
+	static String labelToUri(String label)
+	{
+		return BB2+WordUtils.capitalizeFully(label).replaceAll(" ","");
+	}
+	
+	static String labelToTriple(String label)
+	{
+		return labelToUri(label)+" rdfs:label "+'"'+label.trim()+'"'+"@en.";
+	}
 
 	public static void main(String[] args) throws Docx4JException, JAXBException
 	{
+		MODEL.setNsPrefix("bb2",BB2);
+		MODEL.setNsPrefix("meta",META);
+
+		
 		var wordMLPackage =	Docx4J.load(new java.io.File("../benchmark/input.docx"));
 		var doc = wordMLPackage.getMainDocumentPart();
 		//	https://stackoverflow.com/questions/933447/how-do-you-cast-a-list-of-supertypes-to-a-list-of-subtypes
 		// https://stackoverflow.com/questions/56579819/wt-not-an-instance-of-org-docx4j-wml-text/56580036#56580036
 
 		//var boldText = (List<JAXBElement<Text>>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/w:b]/w:t", false)
-		String[][] tagClasses = {{"w:b","Entity Type"},{"w:i","Role"},{"w:u","Function"}};
+		Object[][] tagClasses = {{"w:b","Entity Type",ENTITY_TYPE},{"w:i","Role",ROLE},{"w:u","Function",FUNCTION}};
 
 		for(var tc: tagClasses)
 		{
-			String tag = tc[0];
-			String clazz = tc[1];
+			String tag = (String)tc[0];
+			String clazz = (String)tc[1];
 			var runs = (List<R>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/"+tag+"]", false);
-			Set<String> names = runs.stream().map(Main::runText).flatMap(Optional::stream).collect(Collectors.toSet());
+			
+			Set<String> names = runs.stream().map(Main::runText).flatMap(Optional::stream)
+					.map(s->s.replaceAll("[^A-Za-z0-9 ]", ""))
+					.filter(s->s.length()>2)
+					.collect(Collectors.toSet());
 
-			System.out.println("*****************"+names.size()+" "+clazz+"s found********"+names.stream().reduce("", (a,b)->a+"\n"+b));
+			System.out.println("*****************"+names.size()+" "+clazz+"s found********");
+			for(String name: names)
+			{
+				Resource r = MODEL.createResource(labelToUri(name),(Resource)tc[2]);
+				r.addLiteral(RDFS.label, MODEL.createLiteral(name, "en"));
+			}
+//					+names.stream()
+//					.map(Main::labelToTriple)
+//					.reduce("", (a,b)->a+"\n"+b));
+			//System.out.println(names.stream().map(Main::labelToUri).reduce("", (a,b)->a+"\n"+b));
 			/*
 		for(R run: boldRuns)
 		{
@@ -54,7 +92,7 @@ public class Main
 				});
 		}
 			 */
-
+			MODEL.write(System.out,"Turtle");
 		}
 	}
 }
