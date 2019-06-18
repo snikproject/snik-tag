aa1b780e32d853e580f6dffb2fcd9636ed2cf8a9
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-06-18 13:55:27 +0200

    Add JavaFX Gui template.

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,134 +1,88 @@
 import java.io.File;
-import java.math.BigInteger;
-import java.util.List;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import org.apache.commons.text.WordUtils;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.vocabulary.RDFS;
-import org.docx4j.Docx4J;
-import org.docx4j.TextUtils;
-import org.docx4j.jaxb.Context;
-import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.wml.CommentRangeEnd;
-import org.docx4j.wml.CommentRangeStart;
-import org.docx4j.wml.Comments.Comment;
-import org.docx4j.wml.ObjectFactory;
-import org.docx4j.wml.R;
-import org.docx4j.wml.R.CommentReference;
-import org.docx4j.wml.Text;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import lombok.SneakyThrows;
 
-
-public class Main
+public class Main extends Application
 {
-	static final Model MODEL = ModelFactory.createDefaultModel();
-	static final String BB2 = "http://www.snik.eu/ontology/bb2/";
-	static final String META = "http://www.snik.eu/ontology/meta/";
-	static final Resource ENTITY_TYPE = MODEL.createResource(META+"EntityType"); 
-	static final Resource FUNCTION = MODEL.createResource(META+"Function");
-	static final Resource ROLE = MODEL.createResource(META+"Role");
-	
- static ObjectFactory factory = Context.getWmlObjectFactory();
- static int commentId = 10000;
 
-	static String labelToUri(String label)
+	@SneakyThrows
+	String openDocx(File file)
 	{
-		return BB2+WordUtils.capitalizeFully(label).replaceAll(" ","");
+		return 	Extractor.extract(file);
 	}
 
-	static String labelToTriple(String label)
-	{
-		return labelToUri(label)+" rdfs:label "+'"'+label.trim()+'"'+"@en.";
-	}
 
-	public static void main(String[] args) throws Docx4JException, JAXBException
-	{
-		MODEL.setNsPrefix("bb2",BB2);
-		MODEL.setNsPrefix("meta",META);
+	@Override
+	public void start(Stage stage) {
+		String javaVersion = System.getProperty("java.version");
+		String javafxVersion = System.getProperty("javafx.version");		
 
-		var wordMLPackage =	Docx4J.load(new java.io.File("../benchmark/input.docx"));
-
-		var doc = wordMLPackage.getMainDocumentPart();
-		List<Comment> comments = doc.getCommentsPart().getContents().getComment();
-		//	 for(Comment c: comments) {System.out.println(c.getContent());}
-		//	https://stackoverflow.com/questions/933447/how-do-you-cast-a-list-of-supertypes-to-a-list-of-subtypes
-		// https://stackoverflow.com/questions/56579819/wt-not-an-instance-of-org-docx4j-wml-text/56580036#56580036
-
-		// put comment ranges into runs so that they don't overlap with commentreferences in the runs
-		var commentRangeStart = (List<JAXBElement<CommentRangeStart>>)(List<?>)doc.getJAXBNodesViaXPath("//w:commentRangeStart", false);
-		var commentRangeEnd = (List<JAXBElement<CommentRangeEnd>>)(List<?>)doc.getJAXBNodesViaXPath("//w:commentRangeEnd", false);
-		for(var el: commentRangeStart)
-		{			
-			var start = el.getValue();
-			var id = start.getId();
-			var endEl = commentRangeEnd.stream().filter(e->e.getValue().getId().equals(id)).findFirst().get();
-			start.get
-		}
-		
-
-		Object[][] tagClasses = {{"w:b","Entity Type",ENTITY_TYPE},{"w:i","Role",ROLE},{"w:u","Function",FUNCTION}};
-
-		for(var tc: tagClasses)
+		var pane = new VBox();
 		{
-			String tag = (String)tc[0];
-			String clazz = (String)tc[1];
-			var runs = (List<R>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/"+tag+"]", false);
+			pane.setAlignment(Pos.CENTER);
+			Scene scene = new Scene(pane, 640, 720);
+			stage.setScene(scene);
+			stage.show();
+		}
+		pane.getChildren().add(new Label(javafxVersion + ", running on Java " + javaVersion + "."));
 
-			for(R run: runs)
-			{
-				String name = TextUtils.getText(run).replaceAll("[^A-Za-z0-9 ]", "");
-				if(name.length()<3) continue;
-				
-				Comment comment = factory.createCommentsComment();
-				comments.add(comment);
-				comment.setId(BigInteger.valueOf(++commentId));
-				Text commentText = factory.createText();
-				commentText.setValue("this is a comment for "+name);
-				comment.getContent().add(commentText);
-				
-				CommentReference commentRef = factory.createRCommentReference();
-				run.getContent().add(commentRef);
-				commentRef.setId(BigInteger.valueOf(commentId));				
+		var openChooser = new FileChooser();		
+		openChooser.setTitle(".docx Datei öffnen");
+		var openButton = new Button(".docx Datei öffnen");
 
-				Model model = ModelFactory.createDefaultModel();
-				Resource r = model.createResource(labelToUri(name),(Resource)tc[2]);
-				r.addLiteral(RDFS.label, MODEL.createLiteral(name, "en"));					
-				MODEL.add(model);				
-			}
 
+		pane.getChildren().add(openButton);
+
+		var rdfText = new TextArea("Ihr extrahierter Text");
+		rdfText.setMinSize(300, 500);		
+
+		openButton.setOnAction(e->
+		{
+			File file = openChooser.showOpenDialog(stage);
+			if(file!=null) {rdfText.setText(openDocx(file));}
+		});
+
+
+		var relationPane = new VBox();
+		{
+			relationPane.setAlignment(Pos.CENTER);
+			Label l = new Label("Ziehen Sie bitte zwei Klassen in die Felder, wählen Sie die passende Relation aus und bestätigen.");
+			relationPane.getChildren().addAll(l);
 		}
 
-		Docx4J.save(wordMLPackage, new File("test.docx"));
-		//		MODEL.write(System.out,"Turtle");
+		{
+			TabPane tabPane = new TabPane();
+			Tab rdfTab = new Tab();
+			{
+				rdfTab.setClosable(false);
+				rdfTab.setText("RDF");
+				rdfTab.setContent(rdfText);
+				tabPane.getTabs().add(rdfTab);
+			}
+			Tab tab = new Tab();
+			{
+				tab.setClosable(false);
+				tab.setText("RDF");
+				tab.setContent(rdfText);
+			}
+			tabPane.getTabs().addAll(rdfTab,tab);
+			pane.getChildren().add(tabPane);
+		}		
 	}
-}
 
-/*
-Set<String> names = runs.stream().map(TextUtils::getText)			
-		.map(s->s.replaceAll("[^A-Za-z0-9 ]", ""))
-		.filter(s->s.length()>2)
-		.collect(Collectors.toSet());
 
-System.out.println("*****************"+names.size()+" "+clazz+"s found********");
-for(String name: names)
-{
-	Resource r = MODEL.createResource(labelToUri(name),(Resource)tc[2]);
-	r.addLiteral(RDFS.label, MODEL.createLiteral(name, "en"));
-}
- */
+	public static void main(String[] args) {
+		launch();
+	}
 
-//					+names.stream()
-//					.map(Main::labelToTriple)
-//					.reduce("", (a,b)->a+"\n"+b));
-//System.out.println(names.stream().map(Main::labelToUri).reduce("", (a,b)->a+"\n"+b));
-/*
-for(R run: boldRuns)
-{
-	runText(run).ifPresent(text->
-	{
-		System.out.println(text);
-	});
 }
- */			
