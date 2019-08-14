4265da0551e8c81961c9fefc6b74a794d80a01f3
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-08-14 14:58:03 +0200

    Begin reworking extractor.

--- src/main/java/eu/snik/tag/Extractor.java
+++ src/main/java/eu/snik/tag/Extractor.java
@@ -1,7 +1,5 @@
 package eu.snik.tag;
-import java.io.File;
 import java.io.InputStream;
-import java.math.BigInteger;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -17,11 +15,8 @@ import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.wml.Comments.Comment;
 import org.docx4j.wml.ObjectFactory;
 import org.docx4j.wml.R;
-import org.docx4j.wml.R.CommentReference;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.docx4j.wml.Text;
 
 /** Extracts SNIK classes from a tagged DOCX file. */
 public class Extractor
@@ -88,54 +83,67 @@ public class Extractor
 		var warnings = new HashSet<String>(); // prevent the same warning from showing multiple times
 		var taggedTexts  = new LinkedHashSet<TaggedText>();
 
-		for(var tc: tagClasses)
+		System.out.println(doc.getContent().stream().reduce((a,b)->a.getClass()+"\n"+b.getClass()).get());
+		doc.getContent().stream().map(o->o).forEachOrdered(run->
 		{
-			String tag = (String)tc[0];
-			var runs = (List<R>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/"+tag+"]", false);
-			runs.removeAll(processedRuns); // we cannot handle overlapping tags right now			
-
-			processedRuns.addAll(runs);
-
-			for(R run: runs)
-			{
-
-				String text = TextUtils.getText(run);
-
-				String label = StringUtils.strip(text,"., ");
-				//label = label.replaceAll("[^A-Za-z0-9 ]", ""); // removing non-alphanumerical characters leads to missing matches in the text tab
-
-				String filterLabel = label.replaceAll("[^A-Za-z0-9 ]", "").replaceAll("(the)|(and)|(or)",""); 
-				if(filterLabel.length()<4&&!filterLabel.matches("[A-Z]{3}")) {continue;} // abbreviations with 3 letters are OK
-				if(filterLabel.length()<3) {continue;} // abbreviations
-
-				/*
-				Comment comment = factory.createCommentsComment();
-				comments.add(comment);
-				comment.setId(BigInteger.valueOf(++commentId));
-				Text commentText = factory.createText();
-				commentText.setValue("this is a comment for "+label);
-				comment.getContent().add(commentText);
-				CommentReference commentRef = factory.createRCommentReference();
-				run.getContent().add(commentRef);
-				commentRef.setId(BigInteger.valueOf(commentId));				
-				 */
-				Clazz clazz = new Clazz(label,labelToLocalName(label),((Subtop)tc[2]));
-				if(processedLabels.contains(label))
-				{
-					classes.stream().filter(c->c.subtop!=clazz.subtop).findAny().ifPresent(c->
-					{
-						warnings.add("Typenkonflikt. Klasse \""+label+"\" ist getagged als sowohl "+c.subtop+" als auch "+clazz.subtop+". "
-								+"Ignoriere "+clazz.subtop+". Bitte beheben Sie den Konflikt im Dokument.");
-					});					
-					continue;
-				}
-				classes.add(clazz);
-				processedLabels.add(label);
-			}
-		}		
-
-		System.err.println(warnings.stream().reduce("", (a,b)->a+"\n"+b));
-		System.out.println(classes.size()+" classes extracted.");
-		return classes;
+			System.out.println("<"+run+">");
+		});
+		
+		return null;
+//
+////		for(var tc: tagClasses)
+////		{
+////			String tag = (String)tc[0];
+//			var runs = (List<R>)(List<?>)doc.getJAXBNodesViaXPath("//w:r[w:rPr/"+tag+"]", false);
+////			runs.removeAll(processedRuns); // we cannot handle overlapping tags right now			
+////
+////			processedRuns.addAll(runs);
+//
+//			for(R run: runs)
+//			{
+//
+//				String text = TextUtils.getText(run);
+//
+//				String label = StringUtils.strip(text,"., ");
+//				//label = label.replaceAll("[^A-Za-z0-9 ]", ""); // removing non-alphanumerical characters leads to missing matches in the text tab
+//
+//				String filterLabel = label.replaceAll("[^A-Za-z0-9 ]", "").replaceAll("(the)|(and)|(or)",""); 
+//				if(filterLabel.length()<4&&!filterLabel.matches("[A-Z]{3}")) {continue;} // abbreviations with 3 letters are OK
+//				if(filterLabel.length()<3)
+//				{
+//					taggedTexts.add(TaggedText.createNormal(text));
+//					continue;
+//				} // abbreviations
+//
+//				/*
+//				Comment comment = factory.createCommentsComment();
+//				comments.add(comment);
+//				comment.setId(BigInteger.valueOf(++commentId));
+//				Text commentText = factory.createText();
+//				commentText.setValue("this is a comment for "+label);
+//				comment.getContent().add(commentText);
+//				CommentReference commentRef = factory.createRCommentReference();
+//				run.getContent().add(commentRef);
+//				commentRef.setId(BigInteger.valueOf(commentId));				
+//				 */
+//				Clazz clazz = new Clazz(label,labelToLocalName(label),((Subtop)tc[2]));
+//				if(processedLabels.contains(label))
+//				{
+//					classes.stream().filter(c->c.subtop!=clazz.subtop).findAny().ifPresent(c->
+//					{
+//						warnings.add("Typenkonflikt. Klasse \""+label+"\" ist getagged als sowohl "+c.subtop+" als auch "+clazz.subtop+". "
+//								+"Ignoriere "+clazz.subtop+". Bitte beheben Sie den Konflikt im Dokument.");
+//					});					
+//					continue;
+//				}
+//				classes.add(clazz);
+//				processedLabels.add(label);
+//			}
+//		}		
+//
+//		System.err.println(warnings.stream().reduce("", (a,b)->a+"\n"+b));
+//		System.out.println(classes.size()+" classes extracted.");
+//		return classes;
 	}
+	
 }
