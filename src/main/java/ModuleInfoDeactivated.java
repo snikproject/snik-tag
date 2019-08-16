77230bf28cd55f417f65448ab177fd152aa73f85
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-08-16 17:36:48 +0200

    Convert to modules but deactivate them because they are too buggy yet.

---
+++ src/main/java/ModuleInfoDeactivated.java
@@ -0,0 +1,24 @@
+/** Using modules has to many bugs still with lombok and also with Junit. When they are fixed and this is needed, rename again to module-info.java and remove commenting. */
+/*
+module eu.snik.tag
+{
+	exports eu.snik.tag.gui;
+	exports eu.snik.tag;
+
+  requires static lombok;
+	requires java.desktop;
+	requires java.logging;
+	requires java.xml.bind;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javatuples;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires org.apache.jena.core;
+	requires org.apache.jena.ext.com.google;
+	requires org.docx4j.core;
+	requires org.docx4j.openxml_objects;
+	requires org.json;	
+}
+*/
