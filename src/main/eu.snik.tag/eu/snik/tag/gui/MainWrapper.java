66a4c05a6bb735f425c3c6351ff081b1572c2b94
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-10-07 14:08:59 +0200

    Add maven assembly plugin to create a runnable jar. Part of #4.

---
+++ src/main/eu.snik.tag/eu/snik/tag/gui/MainWrapper.java
@@ -0,0 +1,8 @@
+package eu.snik.tag.gui;
+
+/** Workaround for the jar execution error "Error: JavaFX runtime components are missing, and are required to run this application".
+ * See https://stackoverflow.com/questions/52653836/maven-shade-javafx-runtime-components-are-missing .*/
+public class MainWrapper
+{
+	public static void main(String[] args) {Main.main(args);}
+}
